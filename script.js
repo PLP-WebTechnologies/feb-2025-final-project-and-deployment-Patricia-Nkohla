50301de32ce2134ed8fcc003d409b0d51992fa59
Patricia-Nkohla <pettynkoh@gmail.com>
2025-05-10 21:05:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showPromo() {
+    alert("🎉 Get 10% off all gadgets this weekend!");
+  }
+  function addToCart(productName) {
+    alert(`${productName} has been added to your cart!`);
+  }
+  
